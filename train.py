e1c07e32450ee9653b1baef55159b21cf31cdae7
Ian Lin <ianrp.lin@gmail.com>
2023-07-27 15:18:50 -0400

    Simplify ETL pipeline

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import pandas as pd
-from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
@@ -15,44 +14,18 @@ def etl_pipeline():
     # Read csv
     loan_data = pd.read_csv('data/loan-approval-data.csv')
 
-    # Remove unnecessary column
-    loan_data = loan_data.drop(['Loan_ID'], axis=1)
+    # Keep required columns
+    loan_data = loan_data[['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount', 'Credit_History', 'Loan_Status']]
 
-    # Map Values
-    # Label encode gender
-    loan_data['Gender'] = loan_data['Gender'].map({'Male': 1, 'Female': 0})
+    # Change income data type
+    loan_data['ApplicantIncome'] = loan_data['ApplicantIncome'].astype(float)
 
-    # label encode education
-    loan_data['Education'] = loan_data['Education'].map({'Graduate': 1, 'Not Graduate': 0})
-
-    # Label encode property area
-    loan_data['Property_Area'] = loan_data['Property_Area'].map({'Rural': 0, 'Semiurban': 1, 'Urban': 2})
-
-    # Encode data
-    # Label Encoder
-    label_encoder = preprocessing.LabelEncoder()
-
-    # Label encode marital status
-    loan_data['Married'] = label_encoder.fit_transform(loan_data['Married'])
-
-    # Label encode employer
-    loan_data['Self_Employed'] = label_encoder.fit_transform(loan_data['Self_Employed'])
-
-    # Imputation
-    # Impute missing dependent values
-    loan_data['Dependents'] = loan_data['Dependents'].fillna(loan_data['Dependents'].median())
-    loan_data['Dependents'] = loan_data['Dependents'].astype(int)
-
-    # Impute missing loan amount and term values
-    loan_data[['LoanAmount', 'Loan_Amount_Term']] = loan_data[['LoanAmount', 'Loan_Amount_Term']].\
-        fillna(loan_data[['LoanAmount', 'Loan_Amount_Term']].median())
+    # Impute missing loan amount
+    loan_data['LoanAmount'] = loan_data['LoanAmount'].fillna(loan_data['LoanAmount'].median())
 
     # Impute missing credit history
     loan_data['Credit_History'] = loan_data['Credit_History'].fillna(loan_data['Credit_History'].median())
     loan_data['Credit_History'] = loan_data['Credit_History'].astype(int)
-
-    # Change income data type
-    loan_data['ApplicantIncome'] = loan_data['ApplicantIncome'].astype(float)
 
     return loan_data
 
@@ -64,10 +37,7 @@ def train(loan_data):
     :param loan_data: cleaned pandas dataframe
     :return: trained random forest mode, features for test data, labels for test data
     """
-    features = loan_data.drop(['Loan_Status'], axis=1)[['ApplicantIncome',
-                                                        'CoapplicantIncome',
-                                                        'LoanAmount',
-                                                        'Credit_History']]
+    features = loan_data.drop(['Loan_Status'], axis=1)
     label = loan_data['Loan_Status']
 
     x_train, x_test, y_train, y_test = train_test_split(features, label, test_size=0.3, random_state=1)
